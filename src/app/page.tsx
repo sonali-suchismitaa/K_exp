1f298de89a47a7d4a1ac210a189eb2605b357485
Debi Prasanna Dash <130679362+debiprasannadash@users.noreply.github.com>
2024-10-02 22:51:36 +0530

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,10 @@
 import Hero_1 from './Components/Hero_1';
-import Hero2 from './Components/Hero_2';
 
 export default function Home() {
   return (
     <main className="">
       <div className="">
-        <Hero1 />
-        <Hero_2/>
+        <Hero_1 />
       </div>
     </main>
   );
