af7bb1b03c84251f50cb3cc9e65ab68718b33f2d
PRÌYÁNSU <85109515+priyansu1001@users.noreply.github.com>
2024-10-04 23:25:18 +0530

    Update page.tsx for fixing deployment

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,22 +1,12 @@
 import Hero_1 from './Components/Hero_1';
-<<<<<<< HEAD
 import Hero2 from './Components/hero2';
 import Hero3 from './Components/Hero3';
-=======
-import hero2 from './Components/hero2';
-import Hero3 from './Components/Hero3';
-
->>>>>>> d0f0e85f5186573c20b9fd890fd50d3558d2ab7c
 export default function Home() {
   return (
     <main className="">
       <div className="">
         <Hero_1 />
-<<<<<<< HEAD
         <Hero2 />
-=======
-        <hero2 />
->>>>>>> d0f0e85f5186573c20b9fd890fd50d3558d2ab7c
         <Hero3 />
       </div>
     </main>
