54af59a2efd7b8f306c21b71d2591991a125f116
Debi Prasanna Dash <130679362+debiprasannadash@users.noreply.github.com>
2024-10-17 19:30:22 +0530

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,6 @@ import Hero3 from './Components/Hero3';
 import Hero_4 from './Components/hero4';
 import Hero_5 from './Components/Hero_5';
 import Footer from './Components/Footer';
-import Login from './Components/Login';
 
 export default function Home() {
   return (
