cb48f682da9cd1f5b868895f8e665403c1053b64
Debi Prasanna Dash <130679362+debiprasannadash@users.noreply.github.com>
2024-10-25 23:09:00 +0530

    Update route.ts

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,22 +1,21 @@
-import NextAuth from "next-auth"
+import NextAuth from "next-auth";
 
-// importing providers
-import GithubProvider from "next-auth/providers/github"
+import GithubProvider from "next-auth/providers/github";
 import GoogleProvider from "next-auth/providers/google";
 
 const handler = NextAuth({
-    providers: [
-        GithubProvider({
-            clientId: process.env.GITHUB_ID as string,
-            clientSecret: process.env.GITHUB_SECRET as string,
-        }),
-        GoogleProvider({
-            clientId: process.env.GOOGLE_ID as string,
-            clientSecret: process.env.GOOGLE_SECRET as string,
-          }),
-    ],
-    debug: true,
-    secret: process.env.NEXTAUTH_SECRET,
-})
+  providers: [
+    GithubProvider({
+      clientId: process.env.GITHUB_ID as string,
+      clientSecret: process.env.GITHUB_SECRET as string,
+    }),
+    GoogleProvider({
+      clientId: process.env.GOOGLE_ID as string,
+      clientSecret: process.env.GOOGLE_SECRET as string,
+    }),
+  ],
+  secret: process.env.NEXTAUTH_SECRET,  // Important for production
+  debug: process.env.NODE_ENV === "development",  // Only enable debug in development
+});
 
-export { handler as GET, handler as POST }
+export { handler as GET, handler as POST };
